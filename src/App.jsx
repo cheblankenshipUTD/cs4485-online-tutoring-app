a7bb5b4f1eb1d93feacf1c77932cdc3040fcb929
cheblankenshipUTD <che.blankenship@utdallas.edu>
2023-03-20 14:04:44 -0500

    Created a footer component

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ import SarahProfileIcon from "./svg/Sarah.svg";
 import RandyProfileIcon from "./svg/Randy.svg";
 import AlexProfileIcon from "./svg/Alex.svg";
 import ShariceProfileIcon from "./svg/Sharice.svg";
+import Footer from "./Footer";
 
 export default class App extends Component {
   render() {
@@ -348,84 +349,8 @@ export default class App extends Component {
             </Row>
           </Container>
         </div>
-
         <br></br>
-
-        <MDBFooter
-          bgColor="headOrange"
-          className="text-center text-lg-start text-white border-top"
-        >
-          <section className="">
-            <MDBContainer className="text-center text-md-start mt-5">
-              <MDBRow className="mt-3">
-                <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4 ">
-                  <h6 className="text-uppercase fw-bold mb-4">
-                    <img src={footerLogo} alt="logo" />
-                  </h6>
-                  <p>
-                    UTD TUTOR mission is to provide students a way to get help
-                    from and to help other students.
-                  </p>
-                </MDBCol>
-
-                <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
-                  <h6 className="text-uppercase fw-bold mb-4">Support</h6>
-                  <p>
-                    <a href="#!" className="text-reset">
-                      Contact Us
-                    </a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-reset">
-                      Suggestions
-                    </a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-reset">
-                      Tutoring Tips
-                    </a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-reset">
-                      Report
-                    </a>
-                  </p>
-                </MDBCol>
-
-                <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
-                  <h6 className="text-uppercase fw-bold mb-4">About Us</h6>
-                  <p>
-                    <a href="#!" className="text-reset">
-                      Our Story
-                    </a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-reset">
-                      Blog
-                    </a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-reset">
-                      News
-                    </a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-reset">
-                      Help
-                    </a>
-                  </p>
-                </MDBCol>
-              </MDBRow>
-            </MDBContainer>
-          </section>
-
-          <div
-            className="text-center p-4"
-            style={{ backgroundColor: "#C9803C" }}
-          >
-            © 2023 UTD TUTOR. All Rights Reserved.
-          </div>
-        </MDBFooter>
+        <Footer />
       </div>
     );
   }
---
+++ src/Footer.jsx
@@ -0,0 +1,90 @@
+import { Component } from "react";
+import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from "mdb-react-ui-kit";
+import footerLogo from "./svg/Footer Logo.svg";
+
+class Footer extends Component {
+    render() {
+        return (
+            <MDBFooter
+                bgColor="headOrange"
+                className="text-center text-lg-start text-white border-top"
+            >
+            <section className="">
+            <MDBContainer className="text-center text-md-start mt-5">
+              <MDBRow className="mt-3">
+                <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4 ">
+                  <h6 className="text-uppercase fw-bold mb-4">
+                    <img src={footerLogo} alt="logo" />
+                  </h6>
+                  <p>
+                    UTD TUTOR mission is to provide students a way to get help
+                    from and to help other students.
+                  </p>
+                </MDBCol>
+
+                <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
+                  <h6 className="text-uppercase fw-bold mb-4">Support</h6>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Contact Us
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Suggestions
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Tutoring Tips
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Report
+                    </a>
+                  </p>
+                </MDBCol>
+
+                <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
+                  <h6 className="text-uppercase fw-bold mb-4">About Us</h6>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Our Story
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Blog
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      News
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Help
+                    </a>
+                  </p>
+                </MDBCol>
+              </MDBRow>
+            </MDBContainer>
+            </section>
+
+            <div
+                className="text-center p-4"
+                style={{ backgroundColor: "#C9803C" }}
+            >
+                © 2023 UTD TUTOR. All Rights Reserved.
+            </div>
+            </MDBFooter>
+        );
+
+    }
+
+
+
+}
+export default Footer;
