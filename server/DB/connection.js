ab3a8e5fb64520e4d8a6663ea56c529fa1ba5187
Jacob1563 <jacob.perez6160@gmail.com>
2023-03-23 17:32:13 -0500

    added .env file

--- server/DB/connection.js
+++ server/DB/connection.js
@@ -1,11 +1,14 @@
 const mysql = require("mysql");
+require("dotenv").config({
+  path: "relative/../../.env",
+});
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+  host: process.env.DB_HOST,
+  port: process.env.PORT,
   database: "tutorDB",
-  user: "root",
-  password: "Database",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 connection.connect(function (error) {
