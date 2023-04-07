4d1f59c0cff5c591b8954168f9214cf0cb1217d8
aBlueBanana <99088106+aBlueBanana@users.noreply.github.com>
2023-04-07 17:40:04 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,77 +2,84 @@ const connection = require("./DB/connection");
 const express = require("express");
 const app = express();
 
-// Test api path
-app.get("/api", (req, res) => {
-  res.json({ users: ["userOne", "userTwo", "userThree"] });
+const cors = require("cors");
+app.use(cors());
 
-  //  Example query
-  /*const sql = "select * from people";
+
+app.use(express.json());
+
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+    const sql = "(SELECT people.people_id, email, password, first_name, last_name, tutor_id as userORtutor_id, about_me, profile_url " +
+      "FROM people, tutors " +
+      "WHERE (people.email = ? AND people.password = ? AND people.people_id = tutors.people_id)) " + 
+      "UNION " +
+      "(SELECT people.people_id, email, password, first_name, last_name, user_id as userORtutor_id, Null as about_me, Null as profile_url " +
+      "FROM people, users " + 
+      "WHERE (people.email = ? AND people.password = ? AND people.people_id = users.people_id));"
+    
+      connection.query(sql, [email, password, email, password], (error, result) => {
+      if (error) {
+        req.setEncoding({error: error})
+      }
+
+      else {
+        if (result.length > 0) { //if the user exists in the database
+          res.send(result);
+        } 
+
+        else {
+        res.send({message: "No records existed, wrong username or password"})
+        }
+      }
+    })
+})
+
+// // List all subjects
+app.get("/subjects", (req, res) => {
+
+  //query
+  const sql = "SELECT * FROM courses";
   connection.query(sql, (error, result) => {
     if (error) throw error;
-    res.json({ tutors: result });
-  });*/
+    
+    res.json({ courses: result });
+  });
+
 });
 
-// List all tutors
+// // List all tutors
 app.get("/tutors", (req, res) => {
-  res.json({
-    tutors: [
-      {
-        name: "John Lee",
-        rate: 4.9,
-        numberOfReviews: 3,
-        courseTitle: "CS 2449",
-        profileURL:
-          "https://raw.githubusercontent.com/cheblankenshipUTD/ml-dataset/main/img/John.png",
-      },
-      {
-        name: "Randy Lee",
-        rate: 1.1,
-        numberOfReviews: 23,
-        courseTitle: "MATH 1337",
-        profileURL:
-          "https://raw.githubusercontent.com/cheblankenshipUTD/ml-dataset/main/img/Sarah.png",
-      },
-      {
-        name: "Sarahg Lee",
-        rate: 2.1,
-        numberOfReviews: 45,
-        courseTitle: "CS 4349",
-        profileURL:
-          "https://raw.githubusercontent.com/cheblankenshipUTD/ml-dataset/main/img/Alex.png",
-      },
-      {
-        name: "Scharic Lee",
-        rate: 4.3,
-        numberOfReviews: 11,
-        courseTitle: "HIST 1301",
-        profileURL:
-          "https://raw.githubusercontent.com/cheblankenshipUTD/ml-dataset/main/img/Randy.png",
-      },
-      {
-        name: "Alex Lee",
-        rate: 2.3,
-        numberOfReviews: 15,
-        courseTitle: "IT 2300",
-        profileURL:
-          "https://raw.githubusercontent.com/cheblankenshipUTD/ml-dataset/main/img/Sharice.png",
-      },
-    ],
+
+  //query
+  const sql = "SELECT DISTINCT first_name, last_name, about_me, profile_url, course_name, start_time, end_time, day_of_the_week FROM people, tutors, courses, tutors_times, tutors_courses WHERE people.people_id = tutors.people_id AND tutors.tutor_id = tutors_times.tutor_id AND tutors_courses.tutor_id = tutors.tutor_id AND tutors_courses.course_id = courses.course_id";
+
+  connection.query(sql, (error, result) => {
+    if (error) throw error;
+    
+    res.json({ tutors: result });
   });
+
 });
 
 // // Show information about one specific tutor
 app.get("/tutors/:id", (req, res) => {
   var tutorID = req.params.id;
 
-  const sql = "SELECT * FROM tutors WHERE tutor_id = ?";
+  //query
+  //const sql = "SELECT DISTINCT first_name, last_name, about_me, profile_url, start_time, end_time, day_of_the_week FROM people, tutors, courses, tutors_times WHERE tutors.tutor_id = ? AND people.people_id = tutors.people_id AND tutors.tutor_id = tutors_times.tutor_id";
+  const sql = "SELECT DISTINCT first_name, last_name, about_me, profile_url, course_name, start_time, end_time, day_of_the_week FROM people, tutors, courses, tutors_times, tutors_courses WHERE tutors.tutor_id = ? AND people.people_id = tutors.people_id AND tutors.tutor_id = tutors_times.tutor_id AND tutors_courses.tutor_id = tutors.tutor_id AND tutors_courses.course_id = courses.course_id";
   connection.query(sql, tutorID, (error, result) => {
     if (error) throw error;
 
     res.json({ tutors: result });
   });
+
 });
+
+// //Show specific information for person's profile depending on if they are a user or a tutor
 
 // // Show 'signup for tutor account'
 // app.get("/tutors/new", (req, res) => {
@@ -81,47 +88,14 @@ app.get("/tutors/:id", (req, res) => {
 // })
 
 // // List all reservations (aka 'appointments')
-app.get("/reservations", (req, res) => {
-  res.json({
-    appointments: [
-      {
-        //unsure if we should have appointment numbers like this, but there's numbers on our figma mockup
-        //need to add user auth, for now, can do hardcoded userIDs, will do those soon
-        appointmentNum: 1,
-        name: "CS 2305 with John",
-        zoomURL: "https://zoom.us/",
-        startTime: "2023-04-17 10:00:00",
-        endTime:  "2023-04-17 11:00:00"
-      },
-      {
-        appointmentNum: 2,
-        name: "ITSS 3311 with Tracey",
-        zoomURL: "https://zoom.us/",
-        startTime: "2023-04-18 08:30:00",
-        endTime:  "2023-04-18 09:30:00"
-      },
-      {
-        appointmentNum: 3,
-        name: "CS 4349 with Alex",
-        zoomURL: "https://zoom.us/",
-        startTime: "2023-04-19 04:00:00",
-        endTime:  "2023-04-19 05:00:00"
-      }
-    ] 
-  });
-});
+// app.get("/reservations", (req, res) => {
+//     res.json({ 'users': ['userOne', 'userTwo', 'userThree'] })
+// })
 
 // // Show information about one specific reservation
-app.get("/reservations/:id", (req, res) => {
-  var appointmentID = req.params.id;
-
-  const sql = "SELECT * FROM appointments WHERE appointment_ID = ?";
-  connection.query(sql, appointmentID, (error, result) => {
-    if(error) throw error;
-
-    res.json({appointments : result});
-  });
-});
+// app.get("/reservations:id", (req, res) => {
+//     res.json({ 'users': ['userOne', 'userTwo', 'userThree'] })
+// })
 
 app.listen(8000, () => {
   console.log("Server running on PORT 8000");
