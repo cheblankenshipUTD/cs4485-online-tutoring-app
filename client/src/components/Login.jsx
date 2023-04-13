6c6c5c45c06645840c01318d0daa03b63a432319
UTDDavid <91843300+UTDDavid@users.noreply.github.com>
2023-04-13 10:06:12 -0500

    Made Login page Appearance

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import {MDBContainer, MDBRow, MDBCol, MDBInput } from 'mdb-react-ui-kit';
+import Button from "react-bootstrap/Button";
 
 const Login = () => {
     // State variables
@@ -27,23 +29,35 @@ const Login = () => {
     }
 
     return (
-      <div className="login-wrapper">
-      <h1 style={{ textAlign: "center" }}>Please Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <p>Email</p>
-          <input type="email" onChange={e => {setEmail(e.target.value)}}/>
-        </label>
-        <label>
-          <p>Password</p>
-          <input type="password" onChange={e => {setPassword(e.target.value)}}/>
-        </label>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-        <h1 style={{color: 'red', fontSize: '15px', textAlign: 'center', marginTop: '20px'}}>{loginStatus}</h1>
-      </form>
-    </div>
+        <MDBContainer className="my-5 rounded-5 login-container">
+        <MDBRow>
+          <MDBCol col='5' className="mb-5">
+              <form className="d-flex flex-column ms-5 me-5" onSubmit={handleSubmit}>
+
+                <div className="text-center pt-5">
+                  <img src="https://raw.githubusercontent.com/cheblankenshipUTD/ml-dataset/main/img/App%20Logo.png"
+                    style={{width: '185px'}} alt="logo" />
+                  <h4 className="mt-1 mb-5 pb-1">UTD TUTOR</h4>
+                </div>
+
+                <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email' required onChange={(e) => setEmail(e.target.value)}/>
+                <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password' required onChange={(e) => setPassword(e.target.value)}/>
+
+                <div className="text-center pt-1 mb-5 pb-1">
+                  <Button className="mb-4 w-100" type="submit" >Sign in</Button>
+                  {/* <a className="text-muted" href="#!">Forgot password?</a> */}
+                </div>
+
+                {/* <div className="d-flex flex-row align-items-center justify-content-center pb-4 mb-4 ">
+                  <p className="mb-0">Don't have an account?</p>
+                  <Button className='mx-2' variant='outline-danger'>Sign Up</Button>
+                </div> */}
+                
+                <h1 style={{color: 'red', fontSize: '15px', textAlign: 'center', marginTop: '20px'}}>{loginStatus}</h1>
+              </form>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
     );
 } 
      
